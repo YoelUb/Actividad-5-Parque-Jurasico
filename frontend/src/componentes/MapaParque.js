5e20b433cfff7ef5e3f0c1d933996b4fd2af7e53
YoelUb <yurqubar@myuax.com>
2025-11-06 15:22:16 +0100

    .

--- frontend/src/componentes/MapaParque.js
+++ frontend/src/componentes/MapaParque.js
@@ -1,40 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import './Parque.css';
 
-
-const bounds = [[0, 0], [820, 660]];
+const bounds = [[0, 0], [820, 660]]; // [Y, X]
 const mapMinZoom = 0;
 const mapMaxZoom = 2;
-const center = [410, 330];
+const center = [410, 330]; // Centro [Y, X]
+
+const gridIdToCoordenadas = (gridId) => {
+    const coords = {
+        'a1': [136, 110],
+        'a2': [136, 330],
+        'a3': [136, 550],
+        'b1': [410, 110],
+        'b2': [410, 330],
+        'b3': [410, 550],
+        'c1': [683, 110],
+        'c2': [683, 330],
+        'c3': [683, 550],
+    };
+    return coords[gridId] || center;
+};
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 
 function MapRefresher({ bounds }) {
   const map = useMap();
-
   useEffect(() => {
-
     const timer = setTimeout(() => {
       map.invalidateSize();
       map.fitBounds(bounds);
     }, 100);
     return () => clearTimeout(timer);
   }, [map, bounds]);
-
   return null;
 }
 
 
 const MapaParque = ({ diseno, enClickRecinto }) => {
 
-    const dinosaurios = diseno?.dinosaurios || [];
-
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-        iconUrl: require('leaflet/dist/images/marker-icon.png'),
-        shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-    });
+    const recintos = diseno?.recintos || [];
 
     return (
         <MapContainer
@@ -54,17 +66,17 @@ const MapaParque = ({ diseno, enClickRecinto }) => {
                 noWrap={true}
             />
 
-            {dinosaurios.map(dino => (
+            {recintos.map(recinto => (
                 <Marker
-                    key={dino.id}
-                    position={[dino.posicion_y, dino.posicion_x]}
+                    key={recinto.grid_id}
+                    position={gridIdToCoordenadas(recinto.grid_id)}
                     eventHandlers={{
-                        click: () => enClickRecinto(dino.id),
+                        click: () => enClickRecinto(recinto.id_dinosaurio),
                     }}
                 >
                     <Popup>
-                        <b>{dino.nombre}</b><br />
-                        Especie: {dino.especie}
+                        <b>{recinto.nombre}</b><br />
+                        Tipo: {recinto.tipo}
                     </Popup>
                 </Marker>
             ))}
